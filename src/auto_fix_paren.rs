44da4cae1fdcbc55aeeb00f58393cd18df4573b5
puripuri2100 <puripuri2100@gmail.com>
2023-08-15 18:32:20 +0900

    一つの分割パターンが3個以上出現する場合にも対応できるようにアルゴリズムを拡張

--- src/auto_fix_paren.rs
+++ src/auto_fix_paren.rs
@@ -13,13 +13,23 @@ pub struct ParenInfo {
 }
 
 /// 分割位置の候補の情報。
-/// 「何文字で分割するのか」を保持する。
+/// 「何文字で何回分割するのか」を保持する。
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct SplitPattern {
+  /// 何文字で分割するか
+  len: usize,
+  /// 何回使うか
+  times: usize,
+}
+
+/// 分割位置の候補の情報。
+/// 「何文字で分割するのか」を保持する。
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub struct SplitPatternList {
   /// 現在採用している候補
   now: usize,
   /// 何文字で分割するのか、の候補
-  pattern_lst: Vec<usize>,
+  pattern_lst: Vec<SplitPattern>,
 }
 
 /// 改め文や読み替え規定文に出現するカギカッコ付きの文章を、
@@ -40,74 +50,96 @@ pub async fn auto_fix_paren(text: &str) -> Option<Vec<String>> {
     }
   }
   let mut now_head: usize = 0;
-  let mut pattern: Vec<SplitPattern> = Vec::new();
+  let mut pattern: Vec<SplitPatternList> = Vec::new();
   while now_head < paren_info_lst.len() - 3 {
     let len_max = (paren_info_lst.len() - now_head) / 2;
     let mut pattern_lst = Vec::new();
-    for i in 1..len_max {
-      let paren_lst_1 = &paren_info_lst[now_head..=now_head + i]
-        .iter()
-        .map(|info| info.clone().v)
-        .collect::<Vec<_>>();
-      let paren_lst_2 = &paren_info_lst[now_head + i + 1..=now_head + i * 2 + 1]
-        .iter()
-        .map(|info| info.clone().v)
-        .collect::<Vec<_>>();
-      if paren_lst_1 == paren_lst_2
-        && paren_lst_1[0] == Paren::Open
-        && paren_lst_1[paren_lst_1.len() - 1] == Paren::Close
-        && paren_lst_2[0] == Paren::Open
-        && paren_lst_2[paren_lst_2.len() - 1] == Paren::Close
-      {
-        pattern_lst.push(i + 1)
+    for len in 2..=len_max {
+      let mut max_times = None;
+      for times in (2..=((paren_info_lst.len() - now_head) / len)).rev() {
+        if max_times.is_none() {
+          let paren_lst_lst = (1..=times)
+            .map(|n| {
+              let pos_start = now_head + len * (n - 1);
+              let pos_end = now_head + len * n - 1;
+              let v = &paren_info_lst[pos_start..=pos_end]
+                .iter()
+                .map(|info| info.clone().v)
+                .collect::<Vec<_>>();
+              v.clone()
+            })
+            .collect::<Vec<_>>();
+          let head = &paren_lst_lst[0];
+          if paren_lst_lst.iter().all(|paren_lst| {
+            paren_lst == head
+              && paren_lst[0] == Paren::Open
+              && paren_lst[paren_lst.len() - 1] == Paren::Close
+          }) {
+            max_times = Some(times);
+          }
+        }
+      }
+
+      // 最短の取得を許される最大の回数から取っていくようにする
+      // 典型的なパターンでは最速で終わり、込み入った例外パターンではより多様な選択を検証できる
+      if let Some(max_times) = max_times {
+        let mut l = (2..=max_times)
+          .rev()
+          .map(|times| SplitPattern { len, times })
+          .collect::<Vec<_>>();
+        pattern_lst.append(&mut l);
       }
     }
+
     if pattern_lst.is_empty() {
       // 次の分割候補がないのでトラックバックする
       if let Some(split_pattern) = pattern.pop() {
         if split_pattern.now < split_pattern.pattern_lst.len() - 1 {
-          now_head -= split_pattern.pattern_lst[split_pattern.now] * 2;
-          now_head += split_pattern.pattern_lst[split_pattern.now + 1] * 2;
-          pattern.push(SplitPattern {
+          now_head -= split_pattern.pattern_lst[split_pattern.now].len
+            * split_pattern.pattern_lst[split_pattern.now].times;
+          now_head += split_pattern.pattern_lst[split_pattern.now + 1].len
+            * split_pattern.pattern_lst[split_pattern.now + 1].times;
+          pattern.push(SplitPatternList {
             now: split_pattern.now + 1,
             pattern_lst: split_pattern.pattern_lst,
           })
         } else {
-          now_head -= split_pattern.pattern_lst[split_pattern.now] * 2;
+          now_head -= split_pattern.pattern_lst[split_pattern.now].len
+            * split_pattern.pattern_lst[split_pattern.now].times;
         }
       } else {
         // 分割位置が定まらないためその旨を返す
         return None;
       }
     } else {
-      let n = pattern_lst[0];
-      now_head += n * 2;
-      pattern.push(SplitPattern {
+      // 分割できたので加える
+      let len = pattern_lst[0].len;
+      let n = pattern_lst[0].times;
+      now_head += len * n;
+      pattern.push(SplitPatternList {
         now: 0,
         pattern_lst,
       });
     }
   }
+
   let mut v = Vec::new();
   let mut paren_pos = 0;
   let mut char_pos = 0;
   let chars = text.chars().collect::<Vec<_>>();
-  for SplitPattern { now, pattern_lst } in pattern.iter() {
-    let len = pattern_lst[*now];
-    let p1_start = paren_info_lst[paren_pos].pos;
-    let p1_end = paren_info_lst[paren_pos + len - 1].pos;
-    let p2_start = paren_info_lst[paren_pos + len].pos;
-    let p2_end = paren_info_lst[paren_pos + len * 2 - 1].pos;
-    let s1 = &chars[char_pos..p1_start].iter().collect::<String>();
-    let s2 = &chars[p1_start..=p1_end].iter().collect::<String>();
-    let s3 = &chars[p1_end + 1..p2_start].iter().collect::<String>();
-    let s4 = &chars[p2_start..=p2_end].iter().collect::<String>();
-    paren_pos += len * 2;
-    char_pos = p2_end + 1;
-    v.push(s1.clone());
-    v.push(s2.clone());
-    v.push(s3.clone());
-    v.push(s4.clone());
+  for SplitPatternList { now, pattern_lst } in pattern.iter() {
+    let len = pattern_lst[*now].len;
+    let times = pattern_lst[*now].times;
+    for n in 1..=times {
+      let start = paren_info_lst[paren_pos + len * (n - 1)].pos;
+      let end = paren_info_lst[paren_pos + (len * n) - 1].pos;
+      let s1 = &chars[char_pos..start].iter().collect::<String>();
+      let s2 = &chars[start..=end].iter().collect::<String>();
+      char_pos = end + 1;
+      v.push(s1.clone());
+      v.push(s2.clone());
+    }
+    paren_pos += len * times;
   }
   let s = &chars[char_pos..].iter().collect::<String>();
   v.push(s.clone());
@@ -208,6 +240,46 @@ async fn check_auto_fix_paren6() {
       "と".to_string(),
       "「な」に「ぬ」ね」".to_string(),
       "の".to_string(),
+    ]
+  );
+}
+
+#[tokio::test]
+async fn check_auto_fix_paren7() {
+  assert_eq!(
+    auto_fix_paren("あ「い」」う「え」」お「か」」き「く」け「こ」さ")
+      .await
+      .unwrap(),
+    vec![
+      "あ".to_string(),
+      "「い」」".to_string(),
+      "う".to_string(),
+      "「え」」".to_string(),
+      "お".to_string(),
+      "「か」」".to_string(),
+      "き".to_string(),
+      "「く」".to_string(),
+      "け".to_string(),
+      "「こ」".to_string(),
+      "さ".to_string()
+    ]
+  );
+}
+
+#[tokio::test]
+async fn check_auto_fix_paren8() {
+  assert_eq!(
+    auto_fix_paren("あ「い」」う「え」」お「か」」き")
+      .await
+      .unwrap(),
+    vec![
+      "あ".to_string(),
+      "「い」」".to_string(),
+      "う".to_string(),
+      "「え」」".to_string(),
+      "お".to_string(),
+      "「か」」".to_string(),
+      "き".to_string()
     ]
   );
 }
